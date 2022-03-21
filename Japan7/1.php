589032582c410b43e1a54f9fbf7391930e6b51f9
Minh Châu <pemidolly@gmail.com>
2022-03-21 21:18:54 +0700

    page1

---
+++ Japan7/1.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	 <meta charset="utf-8">
+	 <meta http-equiv="X-UA-Compatible" content="IE=edge">
+	 <meta name="viewport" content="width=device-width, initial-scale=1">
+	 <meta name="csrf-token" content="64By1VGAMf7iR53MUMS9Z1f7Bx0yZwveURtA3BW2">
+	 <meta property="fb:app_id" content="" />
+	<title>Title of the document</title>
+
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="./css/style.css">
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+	<script type="text/javascript" src="./js/main.js"></script>
+</head>
+
+<body>
+	<div class="project-layout page-1">
+
+		<?php include("header.php"); ?>
+
+		<div class="block-2">
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-md-5">
+						<div class="b-banner-1">
+							<img class="people-1" src="images/people-1.png">
+							<img class="people-2" src="images/people-2.png">
+							<img class="people-3" src="images/people-3.png">
+							<img class="people-4" src="images/people-4.png">
+							<img class="b-img-main" src="images/bg-2.png">
+						</div>
+					</div>
+					<div class="col-2 text-vertical-lr b-content">
+						<div class="hihi">
+							<span class="pt-5">木のぬくもりを生かす住まい</span>
+							<h1 class="m-0 font-weight-bold">笑顔をつくる家づくり</h1>
+							<span>経験豊かな職人がつくる</span>
+						</div>
+					</div>
+					<div class="col-md-5">
+						<div class="b-banner-1">
+							<img class="people-1" src="images/people-5.png">
+							<img class="people-4" src="images/people-6.png">
+							<img class="b-img-main" src="images/bg-2.png">
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<?php include("footer.php"); ?>
+
+	</div>
+</body>
+
+</html>
